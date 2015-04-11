3ba90e9aae7917ff615253d1f9e3400609554f60
Ronnie Brown <ronnievoice@gmail.com>
2015-04-11 14:58:00 -0700

    tests pass for stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,8 +1,33 @@
-// this is what you would do if you liked things to be easy:
-// var stringifyJSON = JSON.stringify;
-
-// but you don't so you're going to write it from scratch:
-
 var stringifyJSON = function(obj) {
-  // your code goes here
+  
+  var type = typeof (obj);
+  if (obj === null) {
+    return 'null';
+  }
+  else if (type === 'number' || type === 'boolean') {
+    return String(obj);
+  }
+  else if (type === 'string') {
+    return '"' + obj + '"';
+  }
+  else if (Array.isArray(obj)) {
+    var result = [];
+    for (var i = 0; i < obj.length; i++) {
+      result.push(stringifyJSON(obj[i]));
+    }
+    return '[' + result.join(',') + ']';
+  }
+  else if (type === 'object') {
+    var strObj = '';
+    for (var key in obj) {
+      var val = obj[key];
+      if (typeof val === 'function' || val === undefined){
+        continue;
+      }
+     strObj = strObj + stringifyJSON(key) + ':' + stringifyJSON(val) + ',';
+   }
+   return '{' + strObj.slice(0, strObj.length -1) + '}';
+  }
 };
+  
+
